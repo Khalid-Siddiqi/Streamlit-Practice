ff164100bfd5d2c4e44278e53c996cc6fee696de
Khalid-Siddiqi <khalid20031016@gmail.com>
2026-02-27 16:08:02 +0500

    add radio

--- streamlit-practice/chapter-two.py
+++ streamlit-practice/chapter-two.py
@@ -10,9 +10,9 @@ use_node = st.checkbox("Use Node.JS")
 if use_node:
     st.write("You also want to use Node.JS, great choice!")
 
-# frontend_types = st.radio("Select a frontend framework:", ["React", "Vue", "Angular"])
-# st.write(f"You selected: {frontend_types}, excellent choice!")
+frontend_types = st.radio("Select a frontend framework:", ["React", "Vue", "Angular"])
+st.write(f"You selected: {frontend_types}, excellent choice!")
 
 
-frontend_types = st.selectbox("Select a frontend framework:", ["React", "Vue", "Angular"])
+frontend_types = st.selectbox("Select A frontend framework:", ["React", "Vue", "Angular"])
 st.write(f"You selected: {frontend_types}, excellent choice!")
