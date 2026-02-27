fd6d75e15e450b6fcc6ec0d919d88234b5835765
Khalid-Siddiqi <khalid20031016@gmail.com>
2026-02-27 16:06:27 +0500

    add

--- streamlit-practice/chapter-two.py
+++ streamlit-practice/chapter-two.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
 st.title("Programming Language App")
+
+
+if st.button("Use JavaScript"):
+    st.success("You Selected JavaScript!")
+
+use_node = st.checkbox("Use Node.JS")
+if use_node:
+    st.write("You also want to use Node.JS, great choice!")
+
+# frontend_types = st.radio("Select a frontend framework:", ["React", "Vue", "Angular"])
+# st.write(f"You selected: {frontend_types}, excellent choice!")
+
+
+frontend_types = st.selectbox("Select a frontend framework:", ["React", "Vue", "Angular"])
+st.write(f"You selected: {frontend_types}, excellent choice!")
